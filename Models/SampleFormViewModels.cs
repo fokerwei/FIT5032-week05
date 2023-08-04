bca5294d919dabd6b7ea39f532300a7de30aacb1
2654 <2654637536@qq.com>
2023-08-04 20:49:48 +0800

    Add project files.

---
+++ Models/SampleFormViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace FIT5032_week05.Models
+{
+    public class SampleFormViewModels
+    {
+    }
+    public class FormOneViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
